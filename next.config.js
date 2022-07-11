d9aeeb1809c598c74ea798b19f6f3f10ba9e4929
Federico Moretti <hello@federicomoretti.dev>
2022-07-11 11:18:13 +0200

    chore: update config to use assetPrefix

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  assetPrefix: '/custom',
+  async rewrites() {
+    return [
+      { source: '/custom/_next/:path*', destination: '/_next/:path*' },
+      { source: '/custom/api/:path*', destination: '/api/:path*' },
+    ];
+  },
+  i18n: { locales: ['en', 'it'], defaultLocale: 'en' },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
